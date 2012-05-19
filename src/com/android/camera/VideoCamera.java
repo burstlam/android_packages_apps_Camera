bc17c04bc648ef9aa390fc452c7159dcebb0f2f4
burstlam <burstlam@gmail.com>
2012-05-19 09:19:10 +0800

    Revert "Camera : Use optimal preview resolution in video recording"
    This reverts commit df8daa4f840b26288a4f09f43630a392311308a5.

--- src/com/android/camera/VideoCamera.java
+++ src/com/android/camera/VideoCamera.java
@@ -796,37 +796,28 @@ public class VideoCamera extends ActivityBase
 
     private void getDesiredPreviewSize() {
         mParameters = mCameraDevice.getParameters();
-        List<Size> sizes = mParameters.getSupportedPreviewSizes();
-        Size preferred = mParameters.getPreferredPreviewSizeForVideo();
-
-        int product = preferred.width * preferred.height;
-        Iterator it = sizes.iterator();
-        // Remove the preview sizes that are not preferred.
-        while (it.hasNext()) {
-            Size size = (Size) it.next();
-            if (size.width * size.height > product) {
-                it.remove();
+        if (mParameters.getSupportedVideoSizes() == null || effectsActive()) {
+            mDesiredPreviewWidth = mProfile.videoFrameWidth;
+            mDesiredPreviewHeight = mProfile.videoFrameHeight;
+        } else {  // Driver supports separates outputs for preview and video.
+            List<Size> sizes = mParameters.getSupportedPreviewSizes();
+            Size preferred = mParameters.getPreferredPreviewSizeForVideo();
+            if (preferred == null) {
+                preferred = sizes.get(0);
             }
-        }
-        Size optimalSize = Util.getOptimalPreviewSize(this, sizes,
-            (double) mProfile.videoFrameWidth / mProfile.videoFrameHeight);
-        // If found optimal resolution is larger than the requested,
-        // set desired resolution to the requested resolution, if it is supported
-        if(optimalSize.width > mProfile.videoFrameWidth || optimalSize.height > mProfile.videoFrameHeight){
-                Iterator tit = sizes.iterator();
-                while (tit.hasNext()) {
-                    Size size = (Size) tit.next();
-                    if (size.width == mProfile.videoFrameWidth &&  size.height == mProfile.videoFrameHeight) {
-                        mDesiredPreviewWidth = mProfile.videoFrameWidth;
-                        mDesiredPreviewHeight = mProfile.videoFrameHeight;
-                        break;
-                    }
+            int product = preferred.width * preferred.height;
+            Iterator it = sizes.iterator();
+            // Remove the preview sizes that are not preferred.
+            while (it.hasNext()) {
+                Size size = (Size) it.next();
+                if (size.width * size.height > product) {
+                    it.remove();
                 }
-        } else {
-         // if found optimal resolution is not greater than
-         // requested resolution , use it.
-             mDesiredPreviewWidth = optimalSize.width;
-             mDesiredPreviewHeight = optimalSize.height;
+            }
+            Size optimalSize = Util.getOptimalPreviewSize(this, sizes,
+                (double) mProfile.videoFrameWidth / mProfile.videoFrameHeight);
+            mDesiredPreviewWidth = optimalSize.width;
+            mDesiredPreviewHeight = optimalSize.height;
         }
         Log.v(TAG, "mDesiredPreviewWidth=" + mDesiredPreviewWidth +
                 ". mDesiredPreviewHeight=" + mDesiredPreviewHeight);
